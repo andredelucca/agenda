7cf4a72d86ace798cdf036d85b37f9be53345cd4
andredelucca <andre.delucca80@gmail.com>
2020-05-15 13:01:00 -0300

    Primeiro commit da aplicação agenda, contemplando as criações das tabelas padrões do Django e a criação do app core

---
+++ Anotacoes_aula.py
@@ -0,0 +1,4 @@
+#no powershell, ativa a virtualenvs, depois cria projeto e depois app
+#no terminal, digitar python manage.py migrate (vai criar tabelas)
+#no terminal, digitar python manage.py createsuperuser --username admin
+    #pede email e depois uma senha
